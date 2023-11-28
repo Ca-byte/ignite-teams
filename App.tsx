5ae23ba301552d1b4b8ff87620e37dfbe0d76d29
Caroline Vieira <carolinesanvieira@gmail.com>
2023-11-28 23:01:35 +0100

    create theme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
+import theme from "@/theme";
+import { ThemeProvider } from "styled-components";
 import {Groups} from "@/screens/Groups";
 
 export default function App() {
   return (
-   <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 
