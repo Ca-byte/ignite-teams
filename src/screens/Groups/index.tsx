837cd67c6a4fc265b8a6219dfb1f548badff7d7a
Caroline Vieira <carolinesanvieira@gmail.com>
2023-12-04 19:24:55 -0400

    group list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@/components/Header';
 import { Container }from './styles';
 import { Highlight } from '@/components/Highlight';
 import { GroupCard } from '@/components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Fetchly Squad']);
   return (
     <Container>
       <Header />
@@ -11,8 +15,15 @@ export function Groups() {
       title='Team'
       subtitle='Play with your team'
       />
-      <GroupCard 
-      title='Fetchly Squad'
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
       />
     </Container>
   );
