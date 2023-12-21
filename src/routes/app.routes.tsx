8591f6499ecd1b2d8da8ebd5695e199b5b67ff47
Caroline Vieira <carolinesanvieira@gmail.com>
2023-12-21 18:50:17 +0100

    implement stack navigator

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,29 @@
+import { Groups } from '@/screens/Groups';
+import { NewGroup } from '@/screens/NewGroup';
+import { Player } from '@/screens/Players';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+
+export function AppRoutes(){
+	return(
+		<Navigator>
+			<Screen 
+			name='groups'
+			component={Groups}
+			/>
+
+			<Screen 
+			name='new'
+			component={NewGroup}
+			/>
+			
+			<Screen 
+			name='players'
+			component={Player}
+			/>
+		</Navigator>
+	)
+}
+
