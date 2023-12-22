c3e8478bfeaaa94783f268bca5a07e6a7b27e952
Caroline Vieira <carolinesanvieira@gmail.com>
2023-12-22 21:47:47 +0100

    surfing

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'; 
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@/components/Loading';
 
 import theme from './src/theme';
 
-import {Groups} from "@/screens/Groups";
+import { Routes } from '@/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +17,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading /> }
+      {fontsLoaded ? <Routes />: <Loading /> }
     </ThemeProvider>
   );
 }
