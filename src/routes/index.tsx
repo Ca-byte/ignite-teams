2f25e5daf87627b8ecc07b8c3b4c630087ec1401
Caroline Vieira <carolinesanvieira@gmail.com>
2023-12-26 21:19:24 +0100

    fix surfing glitch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer} from '@react-navigation/native'
 import { AppRoutes } from './app.routes'
-
 
 export function Routes(){
 	return(
