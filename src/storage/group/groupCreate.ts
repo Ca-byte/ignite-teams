9a54e533f4b5f267b1e4061125902936fbb6bf65
Caroline Vieira <carolinesanvieira@gmail.com>
2023-12-28 16:19:06 +0100

    add error class message

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,11 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "../storageConfig";
 import { groupsGetAll } from "./groupsgetAll";
+import { AppError } from "@/utils/AppError";
 
 export async function groupCreate(newGroupName:string) {
 	try {
-
 		const storedGroups = await groupsGetAll();
+
+		const groupAlreadyExists = storedGroups.includes(newGroupName)
+
+		if (groupAlreadyExists){
+			throw new AppError("Oh no :( There's already a group with this name! Please choose another!")
+		}
 
 		const storage = JSON.stringify([...storedGroups, newGroupName] )
 
